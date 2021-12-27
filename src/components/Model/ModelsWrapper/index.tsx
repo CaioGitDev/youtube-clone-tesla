0ef8f4a608699466b33657c7ab1a8e9634922ab0
CaioDev <53819717+CaioGitDev@users.noreply.github.com>
2021-12-27 01:09:36 +0000

    add hooks

--- src/components/Model/ModelsWrapper/index.tsx
+++ src/components/Model/ModelsWrapper/index.tsx
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, { useCallback, useRef, useState } from 'react';
+import ModelsContext, { CarModel } from '../ModelsContext';
+import ModelOverlay from '../ModelOverlay';
 
-import { Container } from './styles';
+import { Container, OverlaysRoot } from './styles';
 
 const ModelsWrapper: React.FC = ({ children }) => {
+  const wrapperRef = useRef<HTMLDivElement>(null);
+
+  const [registeredModels, setRegisteredModels] = useState<CarModel[]>([]);
+
+  const registerModel = useCallback((model: CarModel) => {
+    setRegisteredModels((prevModels) => [...prevModels, model]);
+  }, []);
+
+  const unregisterModel = useCallback((modelName: string) => {
+    setRegisteredModels(state => state.filter(model => model.modelName !== modelName))
+  }, []);
+
+  const getModelByName = (modelName: string) => {
+    return registeredModels.find(model => model.modelName === modelName) || null;
+  }
+
   return (
-    <Container>
+    <ModelsContext.Provider value={{
+      wrapperRef,
+      registeredModels,
+      registerModel,
+      unregisterModel,
+      getModelByName
+    }}>
+      <Container ref={wrapperRef}> 
+      <OverlaysRoot>
+        {registeredModels.map(item => (
+          <ModelOverlay key={item.modelName} model={item}>
+            {item.overlayNode}
+          </ModelOverlay>
+        ))}
+      </OverlaysRoot>
+
       {children}
     </Container>
+    </ModelsContext.Provider>
   );
 };
 
